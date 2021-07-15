ee85bd0ba2dbdfe749655a8e8ab26d745fef4f8d
Spinmeister <34025631+IlanWanounou@users.noreply.github.com>
2021-07-15 23:44:19 +0200

    #9

---
+++ admin/app/panel/creation.php
@@ -0,0 +1,2 @@
+<?php
+require_once(__DIR__ . '/../../../vues/admin_vues/v_creation.php');
---
+++ vues/admin_vues/v_nav_admin.php
@@ -0,0 +1,45 @@
+<?php
+require_once(__DIR__ . '/../../controleurs/session.php');
+?>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-center navbar-fixed w-100">
+    <a class="navbar-brand" href="v_nav_admin.php">Administration</a>
+    <button class="navbar-toggler mr-auto" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav nav w-100 justify-content-center">
+            <li class="nav-item<?php
+            if ($page == 'creation') {
+                echo ' active';
+            }
+            ?>">
+                <a class="nav-link" href="creation">Creation</a>
+            </li>
+            <li class="nav-item<?php
+            if ($page == 'edit') {
+                echo ' active';
+            }
+            ?>">
+                <a class="nav-link" href="">Edition</a>
+            </li>
+        </ul>
+    </div>
+    <?php
+    if (Session\SessionManager::isConnected()) {
+        ?>
+        <div class="nav-item align-self-center">
+            <a class="btn btn-info btn-sm"  href="../../admin/panel"><i class="fas fa-cog"></i> Gérer le site</a>
+            <a class="btn btn-danger btn-sm" title="Se déconnecter" href="/admin/logout"><i class="fas fa-sign-out-alt"></i></i></a>
+        </div>
+        <?php
+    } else {
+        ?>
+        <div class="nav-item align-self-center">
+            <a class="btn btn-info btn-sm" href="../../admin/login"><i class="fas fa-sign-in-alt"></i> Se connecter</a>
+        </div>
+        <?php
+    }
+    ?>
+</nav>
