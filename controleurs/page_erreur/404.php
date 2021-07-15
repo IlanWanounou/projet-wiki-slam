035e6c6f3629f05cf8d9564c166d25dda036accb
Angelo TREMOUREUX <angelotremoureux.f2f@gmail.com>
2021-07-15 20:43:28 +0200

    #35

--- controleurs/page_erreur/404.php
+++ controleurs/page_erreur/404.php
@@ -2,25 +2,25 @@
 $meta['title'] = 'Erreur 404';
 $meta['css'] = ['bootstrap.min.css', 'fontawesome.all.min.css'];
 $meta['js'] = ['jquery-3.6.0.min.js', 'bootstrap.min.js'];
-require_once(__DIR__ . '/../v_header.php');
+require_once(__DIR__ . '/../../vues/v_header.php');
 ?>
 <body>
 <div class='container text-center'>
 <header>
         <?php
         $page = null;
-        require_once(__DIR__ . '/../v_nav.php');
+        require_once(__DIR__ . '/../../vues/v_nav.php');
         ?>
 </header>
     <div id="content" class="bg-light ">
     <?php
     $error = 404;
-    require_once(__DIR__ . '/../v_error.php');
+    require_once(__DIR__ . '/../../vues/v_error.php');
         ?>
 </div>
 
 <?php
-require_once(__DIR__ . '/../v_footer.php');
+require_once(__DIR__ . '/../../vues/v_footer.php');
 ?>
 </div>
 </body>
