3e0de349896fc1448ee0e9e392fe4dedc1c5e598
Spinmeister <34025631+IlanWanounou@users.noreply.github.com>
2021-07-17 00:46:20 +0200

    #9 #12

--- controleurs/creation.php
+++ controleurs/creation.php
@@ -13,16 +13,29 @@ class CreationDef
     {
         $this->bdd = $bdd;
     }
+    
+    private function uploadImage($image) {
+        $target_dir = "/public/images/uploads/";
+        $target_file = $target_dir . basename($_FILES[$image]["name"]);
+        (move_uploaded_file($_FILES[$image]["tmp_name"], $target_file));
 
-    public function insertArticle($titre, $contenue)
+        return htmlspecialchars( basename( $_FILES[$image]["name"]));
+
+    }
+
+    public function insertArticle($titre, $contenue, $intro, $image)
     {
         try {
             $bdd = $this->bdd;
-            $requete = $bdd->prepare('INSERT INTO article (`id_article`, `titre`, `contenu`, `created_at`) VALUES (NULL, :titre, :contenue, CURRENT_TIMESTAMP)');
+
+            $urlImage = $this->uploadImage($image);
+            $requete = $bdd->prepare('INSERT INTO article (`article_id`, `titre`, `contenue`, `phrase_intro`, `image` `created_at`, `updated_at`, `deleted_at`  ) VALUES (NULL, :titre, :contenue, :intro, :image, CURRENT_TIMESTAMP, NULL , NULL )');
 
             $requete->execute(array(
                 'titre' => $titre,
-                'contenue' => $contenue
+                'contenue' => $contenue,
+                'intro' => $intro,
+                'image' => $urlImage
             ));
             $message = 'Votre article a bien été posté';
             $requete->closeCursor();
