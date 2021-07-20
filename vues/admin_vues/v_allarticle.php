c2eab3dfce62d0a0436b9417e7b961b6ea06df5d
Ilan <wanounouilan@gmail.com>
2021-07-20 17:53:29 +0200

    Mise en forme d'un tableau de tout les articles

--- vues/admin_vues/v_allarticle.php
+++ vues/admin_vues/v_allarticle.php
@@ -4,17 +4,42 @@ require_once(__DIR__ . '/../../controleurs/editArticle.php');
 
 $editArticle = new Article\editArticle($bdd);
 $titreArticle = $editArticle->selectArticle($bdd);
+
+
 ?>
 
-<h1>Edition des articles (en ligne)</h1>
-<div>
-    <ul class="list-unstyled">
+<div class="table-responsive">
+
+    <table class="table table-hover">
+        <thead>
+        <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Article</th>
+            <th scope="col">Editer</th>
+            <th scope="col">Mise en sommeil / Activer</th>
+            <th scope="col">Etat</th>
+        </tr>
+        </thead>
+        <tbody>
         <?php
         foreach ($titreArticle as $titre) {
-            ?>
-            <li>
-                <a href="/admin/articles/edit/<?=$titre['article_id'] ?> "><?= htmlspecialchars($titre['titre']) ?></a>
-            </li>
+        ?>
+        <tr>
+            <th><?= $titre['article_id'] ?></th>
+            <td><?= htmlspecialchars($titre['titre']) ?></td>
+            <td><a href="/admin/articles/edit/<?= $titre['article_id'] ?> ">Editer</a></td>
+            <?php if (!$titre['deleted_at']) { ?>
+                <td><a href="?sommeil=<?= $titre['article_id'] ?>"  type="button">Mettre hors ligne</a></td>
+                <td><span class="badge badge-success ">En ligne</span></td>
+            <?php } else { ?>
+                <td><a href="?sommeil=<?= $titre['article_id'] ?>"  type="button">Mettre en ligne</a></td>
+
+            <td> <span class="badge badge-danger">Hors ligne</span></td>
+            <?php }?>
+        </tr>
         <?php } ?>
-    </ul>
+
+        </tbody>
+    </table>
 </div>
+
