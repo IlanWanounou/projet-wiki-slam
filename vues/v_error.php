c9a583c823f2c541e43e54729afc646eb2faacb8
Ilan <wanounouilan@gmail.com>
2021-07-21 15:32:38 +0200

    Modication msg erreur 403/404

--- vues/v_error.php
+++ vues/v_error.php
@@ -1,7 +1,7 @@
-<h1>ERREUR <?= $error ?></h1>
+<h1 class="pb-4">ERREUR <?= $error ?></h1>
 <?php
 if ($error  === 404) {
-    echo '<h2> On dirai que es perdu </h2>';
+    echo '<h2> La page n\'existe pas ou a été supprimée </h2>';
 ?>
     <a class="btn btn-primary" href="../../index.php" role="button">Revenir à l'accueil</a>
     <div class="row">
@@ -18,9 +18,13 @@ if ($error  === 404) {
 </div>
 <?php
 } else if ($error===403) {
-    echo '<h2> On dirai que tu n\'as pas acces à cette ressource </h2>';
-    echo '<a class="btn btn-primary" href="../../index.php" role="button">Revenir à l\'accueil</a>';
+    echo '<h2> Vous n\'avez pas accès à cette ressource </h2>';
+    ?>
+    <a class="btn btn-primary mt-3" href="../../index.php" role="button">Revenir à l'accueil</a>
+    <div class="row">
+    </div>
 
+<?php
     }
 
 
