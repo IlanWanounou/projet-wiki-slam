5b6ed36e62edf85372e265ac697e696759431bdf
Ilan <wanounouilan@gmail.com>
2021-07-22 16:51:12 +0200

    vue du résulat #65

---
+++ vues/v_recherche.php
@@ -0,0 +1,40 @@
+<?php
+require_once(__DIR__ . '/../controleurs/bdd.php');
+require_once(__DIR__ . '/../controleurs/SearchBar.php');
+
+$recherche = new SearchBar\recherche($bdd);
+if (isset($_GET['q']))
+    $resulat = $recherche->rechecher($_GET['q']);
+
+if (count($resulat) === 0) {
+    ?>
+    <div class="text-center"><h2>Aucun résulat</h2></div>
+<?php } else {
+    ?>
+    <div class="text-center"><h2><?= count($resulat) ?> résulats</h2></div>
+<?php } ?>
+
+<div class="card-group">
+
+<?php
+
+foreach ($resulat as $affiche) {?>
+    <div class="card" style="width: 18rem;">
+    <?php if(Article\ArticleCarousel::getStatusCode('/public/images/uploads/' . $affiche['image']) !== 404) { ?>
+        <img class="card-img-top" src="/public/images/uploads/<?= $affiche['image'] ?>" alt=Image-<?= $affiche['titre'] ?> >
+        <?php } else { ?>
+                <img class="card-img-top" src="/public/images/no_code.png" >
+                <?php } ?>
+
+        <div class="card-body" >
+            <h5 class="card-title" ><?= htmlspecialchars($affiche['titre'])?> </h5 >
+            <p class="card-text" ><?= htmlspecialchars($affiche['phrase_intro']) ?></p>
+            <a href="/article/<?= $affiche['article_id'].'-'.$affiche['titre']?>" class="btn btn-primary">En savoir plus</a>
+        </div>
+    </div>
+
+
+<?php }
+
+
+
