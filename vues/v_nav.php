6d3e70476fc0cc97846db06f887e3b1e84dcaacb
Angelo TREMOUREUX <angelotremoureux.f2f@gmail.com>
2021-07-23 18:20:42 +0200

    fix #71

--- vues/v_nav.php
+++ vues/v_nav.php
@@ -18,7 +18,7 @@ if ($session->isConnected() || !isset($isMaintenance) || isset($isMaintenance) &
     ?>
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-center navbar-fixed w-100">
-        <a class="navbar-brand" href="#">Lexique</a>
+        <a class="navbar-brand" href="/">Lexique</a>
         <button class="navbar-toggler mr-auto" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
