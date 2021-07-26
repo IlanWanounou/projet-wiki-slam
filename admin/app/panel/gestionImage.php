f8ddb0432bc0061121e43bfeef9b555345a1f2d0
Ilan <wanounouilan@gmail.com>
2021-07-26 15:17:10 +0200

    Gestion de l'événement #76

--- admin/app/panel/gestionImage.php
+++ admin/app/panel/gestionImage.php
@@ -3,19 +3,26 @@ $meta['title'] = 'Panel admin - Liste de toutes les images';
 $meta['css'] = ['admin.css'];
 $meta['js'] = ['index.js'];
 
-require_once(__DIR__ . '/../../../controleurs/bdd.php');
-require_once(__DIR__ . '/../../../modele/navbaritems.php');
-require_once(__DIR__ . '/../../../controleurs/adminManager.php');
-require_once(__DIR__ . '/../../../controleurs/articleManager.php');
-require_once(__DIR__ . '/../../../controleurs/articleCarousel.php');
-require_once(__DIR__ . '/../../../controleurs/gestionImage.php');
+use Controller\src\Admin as Admin;
+use Controller\src\Admin\Article\ArticleManager;
+use Controller\src\Admin\Article\EditArticle;
+use Controller\src\Admin\Path\AdminPathManager;
 
-use Services\Admin\Manager\AdminManager as AdminManager;
+require_once(__DIR__ . '/../../../controleurs/modules/bdd.php');
+require_once(__DIR__ . '/../../../modele/admin_pages_config.php');
+require_once(__DIR__ . '/../../../controleurs/src/AdminPathManager.php');
+require_once(__DIR__ . '/../../../controleurs/src/ArticleManager.php');
+require_once(__DIR__ . '/../../../controleurs/src/ArticleCarousel.php');
+require_once(__DIR__ . '/../../../controleurs/src/gestionImage.php');
+require_once(__DIR__ . '/../../../controleurs/src/CreationDef.php');
 
-$currentPagePath = AdminManager::getCurrentPagePath();
-$currentConfigPage = AdminManager::getCurrentPageConfig($admin_pages);
-$articleManager = new Article\ArticleManager($bdd);
+
+
+$currentPagePath = AdminPathManager::getCurrentPagePath();
+$currentConfigPage = AdminPathManager::getCurrentPageConfig($admin_pages);
+$articleManager = new ArticleManager($bdd);
 $gestionImage = new gestion\GestionImage();
+$upload = new Admin\Article\CreationDef($bdd);
 $folder = '../vues/images/uploads/';
 $isEmpty = $gestionImage->isFolderEmpty($folder);
 
@@ -23,16 +30,27 @@ $taileActuel = $gestionImage->getSizeFolder($folder); // En KO
 $taileMax = DIRECTORY_IMAGE_MAXSIZE; // En KO
 
 $allImages = (scandir($folder));
-if(isset($_GET['deleteAll'])) {
+if (isset($_GET['deleteAll'])) {
     $gestionImage->deleteAllImage($folder);
     header('Location: /admin/images');
 }
 if (isset($_GET['delete'])) {
-    $gestionImage->deleteImage($folder.$_GET['delete']);
+    $gestionImage->deleteImage($folder . $_GET['delete']);
     header('Location: /admin/images');
-
 }
 
+if (isset($_FILES['image']) && !empty($_FILES['image'])) {
+    $success = $upload->uploadImage($_FILES['image']);
+    header("Refresh: 5");
+    if ($success) {
+        $resultAdd = '<div class="alert alert-success mt-4" role="alert">
+                    L\'image a été ajouter. </div>';
+    } else {
+        $resultAdd = '<div class="alert alert-danger mt-4" role="alert">
+                    Échec l\'image n\'a pas pu être ajouter. </div>';
+    }
+
+}
 
 
 $loadContentName = 'v_admin_image.php';
